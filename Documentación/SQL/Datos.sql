d20b61d9996fb9d053b3a1becb92eae626939809
Zolmex <zolmexyt@gmail.com>
2025-11-19 14:15:08 -0300

    Creacion de datos para campania_prom

--- Documentación/SQL/Datos.sql
+++ Documentación/SQL/Datos.sql
@@ -21,10 +21,10 @@ VALUES (FALSE, TRUE, TRUE),
        (TRUE, TRUE, TRUE);
 
 INSERT INTO producto_finan (limite_cred, nombre, tasa_base, requisitos_prod_id)
-VALUES (2500000, 'Préstamo Personal', 0.55, 1),
-       (25000000, 'Préstamo Hipotecario', 0.15, 3),
-       (30000000, 'Préstamo Empresarial', 0.3, 3),
-       (10000000, 'Tarjeta Corporativa', 0.45, 2);
+VALUES (2500000, 'Préstamo Personal', 55, 1),
+       (25000000, 'Préstamo Hipotecario', 15, 3),
+       (30000000, 'Préstamo Empresarial', 30, 3),
+       (10000000, 'Tarjeta Corporativa', 45, 2);
 
 INSERT INTO situacion_econ (nombre, ingreso_min, ingreso_max)
 VALUES ('Crítica', 0, 80000),
@@ -39,3 +39,9 @@ VALUES ('DNI'),
        ('Pasaporte'),
        ('CUIT/CUIL'),
        ('Documento Extranjero');
+
+INSERT INTO campania_prom (tasa_prom, vigencia)
+VALUES (18.50, '2026-03-31'),
+       (14.75, '2026-05-15'),
+       (9.9, '2026-09-30'),
+       (0, '2026-04-30');
