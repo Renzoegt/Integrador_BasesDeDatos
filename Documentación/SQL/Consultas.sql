98a570c718d1b5f3d2419d8aa735e6627f1887b7
Emanuel Szkabrij <emaszk2@gmail.com>
2025-11-20 16:28:57 -0300

    carga de datos

--- Documentación/SQL/Consultas.sql
+++ Documentación/SQL/Consultas.sql
@@ -73,3 +73,101 @@ ON Cuota (fecha_venc);
 -- Índice permite filtrar por método de pago y fecha. Su impacto es que facilita realizar informes contables, estadísticas de uso de medios de pago y análisis de cobranzas.
 CREATE INDEX idx_pago_metodo_fecha
 ON Pago (metodo_pago_id, fecha_alta);
+
+-- 11 Subconsulta (Muestra cantidad de créditos aprobados) 
+SELECT c.cliente_id, c.nombre, c.apellido, (
+    SELECT COUNT(*) FROM Solicitud s
+    WHERE s.cliente_id = c.cliente_id
+      AND s.estado = 'aprobado'
+      AND s.credito_id IS NOT NULL
+  ) AS cantidad_creditos_aprobados FROM Cliente c;
+
+-- 12 Subconsulta (Clientes que solicitaron créditos por encima del promedio) CORREGIDO
+SELECT c.cliente_id, c.nombre,
+    c.apellido,
+    s.monto
+FROM Solicitud s
+JOIN Cliente c ON s.cliente_id = c.cliente_id
+WHERE s.monto >
+      (SELECT AVG(monto) FROM Solicitud);
+
+-- 13 CTE (cuantas solicitudes tiene cada cliente) CORREGIDO
+WITH CantSolicitudes AS (
+    SELECT 
+        cliente_id,
+        COUNT(*) AS total_solicitudes
+    FROM Solicitud
+    GROUP BY cliente_id
+)
+
+SELECT 
+    c.cliente_id,
+    c.nombre,
+    c.apellido,
+    cs.total_solicitudes
+FROM CantSolicitudes cs
+JOIN Cliente c ON cs.cliente_id = c.cliente_id
+ORDER BY cs.total_solicitudes DESC;
+
+-- 14 CTE (Obtiene todas las cuotas vencidas y luego en la consulta principal mostrarlas ordenadas.)
+WITH cuotas_vencidas AS (
+    SELECT cuota_id, credito_id, nro_cuota, monto_total, fecha_venc
+    FROM cuota
+    WHERE fecha_venc < CURDATE()
+)
+SELECT *
+FROM cuotas_vencidas
+ORDER BY fecha_venc DESC;
+
+-- 15 HAVING + GROUP BY (Clientes que tienen más de 2 créditos activos y cuya suma total prestada supera $200.000)
+SELECT 
+    cl.cliente_id,
+    cl.nombre,
+    cl.apellido,
+    COUNT(cr.credito_id) AS cantidad_creditos_activos,
+    SUM(cr.monto_ot) AS total_prestado
+FROM Credito cr
+JOIN Solicitud s ON s.credito_id = cr.credito_id
+JOIN Cliente cl ON cl.cliente_id = s.cliente_id
+WHERE cr.habilitado = 1   
+GROUP BY cl.cliente_id, cl.nombre, cl.apellido
+HAVING COUNT(cr.credito_id) > 2
+   AND SUM(cr.monto_ot) > 200000;
+
+-- 16 Ventanas (Ranking de clientes con mayor monto de crédito activo)
+SELECT
+    cl.cliente_id,
+    CONCAT(cl.nombre, ' ', cl.apellido) AS cliente,
+    SUM(cr.monto_ot) AS total_creditos_activos,
+    RANK() OVER (ORDER BY SUM(cr.monto_ot) DESC) AS ranking_monto
+FROM Cliente cl
+JOIN Solicitud s ON s.cliente_id = cl.cliente_id
+JOIN Credito cr ON cr.credito_id = s.credito_id
+WHERE cr.habilitado = TRUE   
+AND s.habilitado = TRUE
+GROUP BY cl.cliente_id, cl.nombre, cl.apellido;
+
+-- 17 Crédito más alto del sistema considerando solo el crédito máximo de cada cliente
+WITH max_por_cliente AS (
+    SELECT
+        cl.cliente_id,
+        CONCAT(cl.nombre, ' ', cl.apellido) AS cliente,
+        MAX(cr.monto_ot) AS credito_max_cliente
+    FROM Cliente cl
+    JOIN Solicitud s ON s.cliente_id = cl.cliente_id
+    JOIN Credito cr ON cr.credito_id = s.credito_id
+    WHERE cr.habilitado = TRUE
+      AND s.habilitado = TRUE
+    GROUP BY cl.cliente_id, cl.nombre, cl.apellido
+),
+ranking_creditos AS (
+    SELECT
+        cliente_id,
+        cliente,
+        credito_max_cliente,
+        RANK() OVER (ORDER BY credito_max_cliente DESC) AS pos
+    FROM max_por_cliente
+)
+SELECT *
+FROM ranking_creditos
+WHERE pos = 1;
